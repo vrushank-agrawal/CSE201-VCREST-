8412c57f7a1eab00403a524043be1f40f7a9827e
Nhat Vo <tonystark0243@gmail.com>
2021-11-11 21:28:33 +0100

    added VideoEditor class

---
+++ src/gui/videoeditor.h
@@ -0,0 +1,28 @@
+//
+// Created by nhatv on 11 Nov 2021.
+//
+
+#ifndef VIDEO_EDITOR_BX23_VIDEOEDITOR_H
+#define VIDEO_EDITOR_BX23_VIDEOEDITOR_H
+
+#include <QMainWindow>
+
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class VideoEditor; }
+QT_END_NAMESPACE
+
+class VideoEditor : public QMainWindow {
+Q_OBJECT
+
+public:
+    explicit VideoEditor(QWidget *parent = nullptr);
+
+    ~VideoEditor() override;
+
+private:
+    Ui::VideoEditor *ui;
+};
+
+
+#endif //VIDEO_EDITOR_BX23_VIDEOEDITOR_H
