24c80fe1dd6876c98c5e3871740bf8203b5dfec8
Nhat Vo <tonystark0243@gmail.com>
2021-11-11 21:49:36 +0100

    added main

--- main.cpp
+++ main.cpp
@@ -1,10 +1,10 @@
 #include <QApplication>
 #include <QPushButton>
+#include "videoeditor.h"
 
 int main(int argc, char *argv[]) {
-    QApplication a(argc, argv);
-    QPushButton button("Hello world!", nullptr);
-    button.resize(200, 100);
-    button.show();
+    QApplication application(argc, argv);
+    VideoEditor videoEditor;
+    videoEditor.show();
     return QApplication::exec();
 }
