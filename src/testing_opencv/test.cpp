62f7e89a0276c84e08551c09db1b609ee93aa517
Vrushank Agrawal <63969942+vrushank-agrawal@users.noreply.github.com>
2021-11-12 18:17:01 +0100

    Run OpenCV successfully and add basic test environment

---
+++ src/testing_opencv/test.cpp
@@ -0,0 +1,27 @@
+//
+// Created by Vrushank on 11/12/2021.
+//
+#include <opencv2/core.hpp>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/highgui.hpp>
+#include <iostream>
+
+using namespace cv;
+
+int main()
+{
+    std::string image_path = samples::findFile("insert img path");
+    Mat img = imread(image_path, IMREAD_COLOR);
+    if(img.empty())
+    {
+        std::cout << "Could not read the image: " << image_path << std::endl;
+        return 1;
+    }
+    imshow("Display window", img);
+    int k = waitKey(0); // Wait for a keystroke in the window
+    if(k == 's')
+    {
+        imwrite("starry_night.png", img);
+    }
+    return 0;
+}
