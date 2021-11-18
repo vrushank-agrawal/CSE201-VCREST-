a24ef09aac006447aae9361db5cb720460c0a39c
Dimitri Korkotashvili <dimitri.korkotashvili@polytechnique.edu>
2021-11-18 13:22:47 +0100

    today's update

--- src/Video/test_video.cpp
+++ src/Video/test_video.cpp
@@ -1,31 +1,57 @@
-//
-// Created by Vrushank on 11/12/2021.
-//
 #include <opencv2/core.hpp>
 #include <opencv2/imgcodecs.hpp>
 #include <opencv2/highgui.hpp>
+#include <opencv2/opencv.hpp>
 #include <iostream>
+#include <string>
 
 using namespace std;
 using namespace cv;
 
-int main()
-{
-    cout << "Video team is working :))" << endl;
-    // whenever you are done with your opencv undo the comments below and test it
-    /*
-    std::string image_path = samples::findFile("copy the image path you want to show");
-    Mat img = imread(image_path, IMREAD_COLOR);
-    if(img.empty())
-    {
-        std::cout << "Could not read the image: " << image_path << std::endl;
-        return 1;
+int main(){
+
+
+    string video_name = "write your video name";
+//example below
+//    string video_name = "D:\\Dimitri\\Homeworks\\YEAR2\\CSE201_Project\\New folder\\video_editor_BX23\\src\\video\\Toilet_Paper.mp4";
+
+    VideoCapture cap(video_name);
+
+    // Check if camera opened successfully
+    if(!cap.isOpened()){
+        cout << "Error opening video stream" << endl;
+        return -1;
     }
-    imshow("Display window", img);
-    int k = waitKey(0); // Wait for a keystroke in the window
-    if(k == 's')
-    {
-        imwrite("starry_night.png", img);
-    }*/
+
+    // we will use this part later
+    //string imgpath = "write image name";
+    //Mat img = imread(imgpath);
+    int frame_width = cap.get(CAP_PROP_FRAME_WIDTH);;
+    int frame_height = cap.get(CAP_PROP_FRAME_HEIGHT);
+
+    // Define the codec and create VideoWriter object.The output is stored in 'outcpp.avi' file.
+    VideoWriter video("outcpp.avi", VideoWriter::fourcc('M','J','P','G'), 10, Size(frame_width,frame_height));
+    while(1){
+
+        Mat frame;
+
+        cap >> frame;
+        if (frame.empty())
+            break;
+        video.write(frame);
+
+        imshow( "Frame", frame);
+
+        char c = (char)waitKey(1);
+        if( c == 27 )
+            break;
+    }
+    cap.release();
+    video.release();
+
+    destroyAllWindows();
+
+    // in the end the avi file must be created and you must see the video
+    cout << "Everything done, opencv works" << endl;
     return 0;
 }
