e57fe2517998e3485c9e6d6306838a9bf792ee44
yao-creative <yytanwork@gmail.com>
2021-11-18 10:52:18 +0100

    hstitch vstitch added.

--- src/Image/image.cpp
+++ src/Image/image.cpp
@@ -56,6 +56,40 @@ void img::Image::imgPreview(const String &window) {
 void img::Image::imgModifiedPreview(const string &window) {
     Mat mat = this -> getModifiedImg();
     imshow(window, mat);
+// Mat stitch(int column_div, int row_div, int num,...){ 
+//     // list of row and column divisions in tuples, column divisions list((int left_most_pixel, int right_most_pixel))
+//     // num indicates the number of image arguments (probably should be limited to 4 max, which can be called recursively
+//     va_list valist;
+//     va_start(valist, num);
+//     Image image_list[num];
+//     Mat output = Mat()
+//     for (i = 0; i < num; i++) {
+//         int width = column_div[i][1] - column_div[i][0]
+//         int height = row_div[i][1] - row_div[i][0]
+//         image_list = va_arg(valist, int).resize(width , height);
+//    }
+//    //use hconcat and vconcat on the list of matrices to create a collage : D
+
+//    for 
+   
+   
+
+// }
+
+//There are only two methods of stitching divide horizontally or vertically in half.
+Mat hstitch(Mat mat1, Mat mat2){
+    //later on we can add the division (currently)
+    Mat output;
+    int out_height = min(mat1.size().height, mat2.size().height);
+    Mat temp1= mat1;
+    Mat temp2= mat2; 
+    //allocate temp1 and temp2 matrices to concatenate and then delete them later. so that we don't modify the original images.
+    temp1.resize(mat1.size().width , out_height);
+    temp2.resize(mat2.size().width, out_height);
+    hconcat(temp1,temp2, output);
+    delete temp1;
+    delete temp2;
+    return output;
 }
 
 //
