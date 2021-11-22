b96c9467e6ec99e2cb97b0117591673b7e73a170
Dimitri Korkotashvili <dimitri.korkotashvili@polytechnique.edu>
2021-11-22 22:12:41 +0100

    Small changes

--- src/Video/test_video.cpp
+++ src/Video/test_video.cpp
@@ -43,9 +43,9 @@ using namespace cv;
 int main(){
 
 
-    string imgpath = "D:\\Dimitri\\Homeworks\\YEAR2\\CSE201_Project\\New folder\\video_editor_BX23\\src\\video\\A.png";
+    //string imgpath = "D:\\Dimitri\\Homeworks\\YEAR2\\CSE201_Project\\New folder\\video_editor_BX23\\src\\video\\A.png";
     Mat img1 = imread(imgpath);
-    imgpath = "D:\\Dimitri\\Homeworks\\YEAR2\\CSE201_Project\\New folder\\video_editor_BX23\\src\\video\\B.png";
+    //imgpath = "D:\\Dimitri\\Homeworks\\YEAR2\\CSE201_Project\\New folder\\video_editor_BX23\\src\\video\\B.png";
     Mat img2 = imread(imgpath);
     Mat images[10];
     int times[10];
