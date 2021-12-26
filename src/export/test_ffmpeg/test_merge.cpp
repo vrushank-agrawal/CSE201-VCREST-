778e289f785ab88c86355b67719e7628c56290f2
Vrushank Agrawal <vrushank2001@gmail.com>
2021-12-26 12:11:31 +0100

    update directory paths

--- src/export/test_ffmpeg/test_merge.cpp
+++ src/export/test_ffmpeg/test_merge.cpp
@@ -5,37 +5,37 @@
 #include "../export.h"
 
 std::string get_curr_dir() {
-    char tmp[256];
-    getcwd(tmp, 256);
-
-    // convert char to string
-    std::string s;
-    s += tmp;
-
-    return s;
-}
-
-int main() {
-
     char add[256];
     getcwd(add, 256);
 
     // convert char to string
     std::string address;
-    address += add;
+    for (int i =0; i< strlen(add); i++){
+        address += add[i];
+    }
 
-    //remove \cmake-build-debug from string
-    // get the last folder in the current working directory
-    char* last_backslash = strrchr(add, '\\');
-//    address.erase(remove(address.begin(), address.end(), "\cmake-build-debug"), address.end());
+    return address;
+}
 
-//    cout << "Current working directory: " << last_backslash << endl;
-    char video_path[200];
-    strcpy(video_path, "/Users/minhtung0404/Documents/CS/github/video_editor_BX23/src/export/test_ffmpeg/test_files/video.mp4");
-    char audio_path[200];
-    strcpy(audio_path, "/Users/minhtung0404/Documents/CS/github/video_editor_BX23/src/export/test_ffmpeg/test_files/audio.wav");
-    char export_address[200];
-    strcpy(export_address, "/Users/minhtung0404/Documents/CS/github/video_editor_BX23/src/export/test_ffmpeg/test_files/output.mkv");
+int main() {
+
+    std::string address = get_curr_dir();
+
+    std::string audio_path;
+    std::string video_path;
+    std::string export_address;
+
+    audio_path = address + R"(\..\test_files\audio.wav)";
+    video_path = address + R"(\..\test_files\video.mp4)";
+    export_address = address + R"(\..\test_files\output.mkv)";
+
+//    char video_path[200];
+//    strcpy(video_path, "/Users/minhtung0404/Documents/CS/github/video_editor_BX23/src/export/test_ffmpeg/test_files/video.mp4");
+//    char audio_path[200];
+//    strcpy(audio_path, "/Users/minhtung0404/Documents/CS/github/video_editor_BX23/src/export/test_ffmpeg/test_files/audio.wav");
+//    std::string audio_path = address;
+//    char export_address[200];
+//    strcpy(export_address, "/Users/minhtung0404/Documents/CS/github/video_editor_BX23/src/export/test_ffmpeg/test_files/output.mkv");
 
 
     exp::exporter(video_path, audio_path, export_address);
