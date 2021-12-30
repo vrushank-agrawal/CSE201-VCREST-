6822a9c54f2321ecef93ae84c25dd5da5a0b2d0f
Lasha Koroshinadze <lashakoroshinadze199@gmail.com>
2021-12-30 01:25:43 +0100

    Optimized headers and imports

--- main.cpp
+++ main.cpp
@@ -4,10 +4,10 @@
 #include "Audio.hpp"
 
 int main(int argc, char *argv[]) {
-
+    //
     audio::Audio a("C:/Users/lasha/Documents/GitHub/video_editor_BX23/media/audio/test.wav");
     audio::Audio b("C:/Users/lasha/Documents/GitHub/video_editor_BX23/media/audio/test1.wav");
     audio::merge(a, b);
     return 0;
-
+    //
 }
