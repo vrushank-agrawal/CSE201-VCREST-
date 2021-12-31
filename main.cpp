4d4e8bdb5ad39edfee7c32e38e744aebba1e3e92
Nhat Vo <tonystark0243@gmail.com>
2021-12-31 15:47:11 +0100

    moved test into Audio.cpp

--- main.cpp
+++ main.cpp
@@ -1,32 +1,15 @@
 #include <QApplication>
 #include "videoeditor.h"
 
-#include "Audio.hpp"
+#include "Audio.cpp"
 
-int main(int argc, char *argv[]) {
-    // First audio file
-    audio::Audio a("../media/audio/test1.mp3");
-    // Second audio file
-    audio::Audio b("../media/audio/test2.wav");
-    /*
-     * These create raw audio files in the folder "cmake-build-debug\temp_media"
-     * output0.wav and output1.wav respectively
-    */
+int main(int argc, char *argv[]){
+    // uncomment to test audio
+//    audio::test();
 
-    // Merges two audio files, creates output2.wav and returns the name of the file
-    std::string urlm = audio::merge(a, b);
-
-    // Merged audio
-    audio::Audio c(urlm);
-    // Trim first 25 seconds of the audio
-    std::string urlt1 = audio::trim(c, 25000, false);
-
-    // Trimmed audio
-    audio::Audio d(urlt1);
-
-    // Leave only first 10 seconds of the audio
-    audio::trim(d, 10000, true);
-
-    return 0;
-
+    QApplication application(argc, argv);
+    application.setStyleSheet("QToolTip { color: #000000; background-color: #ffffff; border: 0px; }");
+    VideoEditor videoEditor;
+    videoEditor.show();
+    return QApplication::exec();
 }
--- src/audio/Audio.cpp
+++ src/audio/Audio.cpp
@@ -192,4 +192,29 @@ namespace audio {
 
     }
 
+    void test() {
+        // First audio file
+        audio::Audio a("../media/audio/test1.mp3");
+        // Second audio file
+        audio::Audio b("../media/audio/test2.wav");
+        /*
+         * These create raw audio files in the folder "cmake-build-debug\temp_media"
+         * output0.wav and output1.wav respectively
+        */
+
+        // Merges two audio files, creates output2.wav and returns the name of the file
+        std::string urlm = audio::merge(a, b);
+
+        // Merged audio
+        audio::Audio c(urlm);
+        // Trim first 25 seconds of the audio
+        std::string urlt1 = audio::trim(c, 25000, false);
+
+        // Trimmed audio
+        audio::Audio d(urlt1);
+
+        // Leave only first 10 seconds of the audio
+        audio::trim(d, 10000, true);
+    }
+
 }
