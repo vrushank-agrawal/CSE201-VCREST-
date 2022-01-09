e24fea482c39ff9824c2e203daca3eb6eccb9ddb
Lasha Koroshinadze <lashakoroshinadze199@gmail.com>
2022-01-09 14:16:36 +0100

    Cleaned main.cpp

--- main.cpp
+++ main.cpp
@@ -3,17 +3,9 @@
 
 #include "Audio.hpp"
 
-#include<iostream>
-
 int main(int argc, char *argv[]) {
     // uncomment to test audio
     // audio::test();
-
-    audio::Audio a("C:/Users/lasha/Documents/GitHub/video_editor_BX23/media/audio/test2.wav");
-
-    for (auto x: a.getBeatPositions()) {
-        std::cout << x << std::endl;
-    }
 
     QApplication application(argc, argv);
     application.setStyleSheet("QToolTip { color: #000000; background-color: #ffffff; border: 0px; }");
